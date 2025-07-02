883590a1901011a9d5f960404c70f6b26f352217
Cleverson Palma <cleversonpalmas@gmail.com>
2025-07-02 17:32:19 +0000

    Correções para deploy a partir do commit ee6c5e4

---
+++ src/pages/Orcamento.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Orcamentos() {
+  return (
+    <div>
+      <h1>Orçamentos</h1>
+      <p>Página funcionando!</p>
+    </div>
+  );
+}
+
+export default Orcamentos;
